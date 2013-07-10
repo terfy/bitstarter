4fa23ad0e28270293e5f52b982c1388bf6df1275
terfy <terfy>
2013-07-10 11:35:28 +0200

    Read from index.html

--- web.js
+++ web.js
@@ -5,9 +5,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   fs.readFileSync('index.html', function (err, data) {
-    if (err) throw err;
     response.send(buf.toString(data)); 
-  }
+  })
 });
 
 var port = process.env.PORT || 5000;
